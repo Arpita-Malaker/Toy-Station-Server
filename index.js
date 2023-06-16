d026f68660e87184f66893b75731645d1764cd55
Arpita-Malaker <arpita.uiu@gmail.com>
2023-06-16 14:13:13 +0600

    redirect to login page

--- index.js
+++ index.js
@@ -61,6 +61,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
          })
+
+         // toys data with id
+            app.get('/toys/:id', async(req,res)=>{
+            const result = await toyCollection.find().toArray();
+            res.send(result);
+         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
